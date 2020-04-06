a3ad1968e55ad7e52040ca104581294638d93566
Derek Colley <derek@netsimple.net>
2020-04-06 15:55:20 +0100

    indent for cells

--- src/me/colley/mpp2xls/config/xml/MyCellStyle.java
+++ src/me/colley/mpp2xls/config/xml/MyCellStyle.java
@@ -2,4 +2,12 @@ package me.colley.mpp2xls.config.xml;
 
 public class MyCellStyle {
 
+	public short indentation;
+	
+	public MyCellStyle(String t) {
+		switch(t) {
+			case "indentation": 
+				this.indentation = 2;
+		}
+	}
 }
